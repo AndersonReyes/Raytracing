08143e9549dac085f6df0c9e72acef08a8c1fb66
Anderson Reyes <reyesanderson428@gmail.com>
2022-08-06 23:04:45 -0400

    separate vec3

---
+++ RTOneWeekend/src/vec3.cc
@@ -0,0 +1,90 @@
+#include "vec3.h"
+
+namespace raytracer {
+const double Vector3::GetX() const {
+  return elem[0];
+}
+
+const double Vector3::GetY() const {
+  return elem[1];
+}
+
+const double Vector3::GetZ() const {
+  return elem[2];
+}
+
+Vector3 Vector3::operator-() const {
+  return Vector3{-elem[0], -elem[1], -elem[2]};
+}
+
+const double Vector3::operator[](int i) const {
+  assert(0 <= i && i <= 2);
+  return elem[i];
+}
+
+Vector3& Vector3::operator+=(const Vector3& v) {
+  elem[0] += v[0];
+  elem[1] += v[1];
+  elem[2] += v[2];
+
+  return *this;
+}
+
+Vector3& Vector3::operator/=(const Vector3& v) {
+  elem[0] /= v[0];
+  elem[1] /= v[1];
+  elem[2] /= v[2];
+
+  return *this;
+}
+
+Vector3& Vector3::operator/=(double t) {
+  elem[0] /= t;
+  elem[1] /= t;
+  elem[2] /= t;
+
+  return *this;
+}
+
+Vector3& Vector3::operator*=(const Vector3& v) {
+  elem[0] *= v[0];
+  elem[1] *= v[1];
+  elem[2] *= v[2];
+
+  return *this;
+}
+
+const double Vector3::Length() const {
+  return std::sqrt(LengthSquared());
+}
+
+const double Vector3::LengthSquared() const {
+  return elem[0] * elem[0] + elem[1] * elem[1] + elem[2] * elem[2];
+}
+
+const double Vector3::Dot(const Vector3& u, const Vector3& v) {
+  return u[0] * v[0] + u[1] * v[1] + u[2] * v[2];
+}
+
+const Vector3 Vector3::Cross(const Vector3& u, const Vector3& v) {
+  return Vector3(u.elem[1] * v.elem[2] - u.elem[2] * v.elem[1],
+                 u.elem[2] * v.elem[0] - u.elem[0] * v.elem[2],
+                 u.elem[0] * v.elem[1] - u.elem[1] * v.elem[0]);
+}
+
+Vector3 operator+(const Vector3& u, const Vector3& v) {
+  return Vector3{u[0] + v[0], u[1] + v[1], u[2] + v[2]};
+}
+
+Vector3 operator-(const Vector3& u, const Vector3& v) {
+  return Vector3{u[0] - v[0], u[1] - v[1], u[2] - v[2]};
+}
+
+Vector3 operator*(const Vector3& u, const Vector3& v) {
+  return Vector3{u[0] * v[0], u[1] * v[1], u[2] * v[2]};
+}
+
+Vector3 operator/(const Vector3& u, const Vector3& v) {
+  return Vector3{u[0] / v[0], u[1] / v[1], u[2] / v[2]};
+}
+}  // namespace raytracer
